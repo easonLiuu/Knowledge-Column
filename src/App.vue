a8b7ffa11a68f381ea5c89aee2a393188bf8ed7e
liujiarui <ljr3289595084@163.com>
2023-03-14 14:47:07 +0800

    feat: add home and login views

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,33 @@
 <template>
    <div class="container">
       <global-header :user="currentUser"></global-header>
-      <column-list :list="list"></column-list>
-      <div class="mb-3">
-        <label class="form-label">邮箱地址</label>
-        <validate-input
-          :rules="emailRules"
-          v-model="emailVal"
-          placeholder="请输入邮箱"
-          type="text"
-        ></validate-input>
-      </div>
-      <div class="mb-3">
-        <label class="form-label">密码</label>
-        <validate-input
-          :rules="passwordRules"
-          v-model="passwordVal"
-          placeholder="请输入密码"
-          type="password"
-        ></validate-input>
-      </div>
+      <home></home>
+      <login></login>
    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ColumnList, { ColumnProps } from './components/ColumnList.vue'
-import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
 
 const currentUser: UserProps = {
   isLogin: true,
   name: 'eason'
 }
 
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧'
-    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
-]
-
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList,
     GlobalHeader,
-    ValidateInput
+    Home,
+    Login
   },
   setup () {
-    const emailVal = ref('')
-    const passwordVal = ref('')
-    const emailRules: RulesProp = [
-      { type: 'required', message: '电子邮箱地址不能为空' },
-      { type: 'email', message: '请输入正确的电子邮箱格式' }
-    ]
-    const passwordRules: RulesProp = [
-      { type: 'required', message: '密码不能为空' }
-    ]
     return {
-      list: testData,
-      currentUser,
-      emailRules,
-      passwordRules,
-      emailVal,
-      passwordVal
+      currentUser
     }
   }
 })
---
+++ src/views/Login.vue
@@ -0,0 +1,70 @@
+<template>
+   <div class="container">
+      <validate-form @form-submit="onFormSubmit">
+        <div class="mb-3">
+        <label class="form-label">邮箱地址</label>
+        <validate-input
+          :rules="emailRules"
+          v-model="emailVal"
+          placeholder="请输入邮箱"
+          type="text"
+          ref="inputRef"
+        ></validate-input>
+      </div>
+      <div class="mb-3">
+        <label class="form-label">密码</label>
+        <validate-input
+          :rules="passwordRules"
+          v-model="passwordVal"
+          placeholder="请输入密码"
+          type="password"
+        ></validate-input>
+      </div>
+      <template #submit>
+        <span class="btn btn-danger">Submit</span>
+      </template>
+      </validate-form>
+   </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ValidateInput, { RulesProp } from '../components/ValidateInput.vue'
+import ValidateForm from '../components/ValidateForm.vue'
+
+export default defineComponent({
+  name: 'Login',
+  components: {
+    ValidateInput,
+    ValidateForm
+  },
+  setup () {
+    const inputRef = ref<any>()
+    const emailVal = ref('')
+    const passwordVal = ref('')
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ]
+    const passwordRules: RulesProp = [
+      { type: 'required', message: '密码不能为空' }
+    ]
+    const onFormSubmit = (result: boolean) => {
+      // console.log(inputRef.value.validateInput())
+    }
+    return {
+      emailRules,
+      passwordRules,
+      emailVal,
+      passwordVal,
+      onFormSubmit,
+      inputRef
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
