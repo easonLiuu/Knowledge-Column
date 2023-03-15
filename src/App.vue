975af7523db049da524692fa7134a6de3f8f90cd
liujiarui <ljr3289595084@163.com>
2023-03-15 15:23:12 +0800

    feat: add Loader component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="container">
       <global-header :user="currentUser"></global-header>
-      <h1 v-if="isLoading">正在读取</h1>
+      <loader v-if="isLoading" text="拼命加载中" background="rgba(0, 0, 0, 0.8)"></loader>
       <router-view></router-view>
       <footer class="text-center py-4 text-secondary bg-light mt-6">
       <small>
@@ -22,11 +22,13 @@ import { computed, defineComponent } from 'vue'
 import { useStore } from 'vuex'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import GlobalHeader from './components/GlobalHeader.vue'
+import Loader from './components/Loader.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    GlobalHeader
+    GlobalHeader,
+    Loader
   },
   setup () {
     const store = useStore()
