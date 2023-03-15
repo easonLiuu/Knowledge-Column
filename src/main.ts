c234d5e8e47193a8b38a20adcb76bd335d974b56
liujiarui <ljr3289595084@163.com>
2023-03-15 13:58:12 +0800

    feat: add vuexaction

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { createApp } from 'vue'
+import axios from 'axios'
 import store from './store'
 import App from './App.vue'
 import router from './router'
 
+axios.interceptors.request.use(config => {
+  store.commit('setLoading', true)
+  return config
+})
+axios.interceptors.response.use(config => {
+  store.commit('setLoading', false)
+  return config
+})
 const app = createApp(App)
 app.use(router)
 app.use(store)
--- src/store.ts
+++ src/store.ts
@@ -1,28 +1,47 @@
-import { createStore } from 'vuex'
-import { testData, testPosts, ColumnProps } from './testData'
-export interface PostProps {
-    id: number;
-    title: string;
-    content: string;
-    image?: string;
-    createdAt: string;
-    columnId: number;
-}
+import { createStore, Commit } from 'vuex'
+import axios from 'axios'
+
 export interface UserProps {
     isLogin: boolean;
     name?: string;
     id?: number;
     columnId?: number
 }
+interface ImageProps {
+    _id?: string;
+    url?: string;
+    createAt?: string;
+}
+export interface PostProps {
+    _id: string;
+    title: string;
+    excerpt?: string;
+    content?: string;
+    image?: ImageProps;
+    createdAt: string;
+    column: string;
+}
+export interface ColumnProps {
+    _id: string;
+    title: string;
+    avatar?: ImageProps;
+    description: string;
+}
 export interface GlobalDataProps {
+    loading: boolean;
     columns: ColumnProps[];
     posts: PostProps[];
     user: UserProps
 }
+const getAndCommit = async (url: string, mutationName: string, commit: Commit) => {
+  const { data } = await axios.get(url)
+  commit(mutationName, data)
+}
 const store = createStore<GlobalDataProps>({
   state: {
-    columns: testData,
-    posts: testPosts,
+    loading: false,
+    columns: [],
+    posts: [],
     user: { isLogin: true, name: 'eason', columnId: 1 }
   },
   mutations: {
@@ -31,17 +50,37 @@ const store = createStore<GlobalDataProps>({
     },
     createPost (state, newPost) {
       state.posts.push(newPost)
+    },
+    fetchColumns (state, rawData) {
+      state.columns = rawData.data.list
+    },
+    fetchColumn (state, rawData) {
+      state.columns = rawData.data
+    },
+    fetchPosts (state, rawData) {
+      state.posts = rawData.data.list
+    },
+    setLoading (state, status) {
+      state.loading = status
+    }
+  },
+  actions: {
+    fetchColumns ({ commit }) {
+      getAndCommit('/api/columns.json', 'fetchColumns', commit)
+    },
+    fetchColumn ({ commit }, cid) {
+      getAndCommit('/api/column.json', 'fetchColumn', commit)
+    },
+    fetchPosts ({ commit }, cid) {
+      getAndCommit(`/column/${cid}/posts`, 'fetchPosts', commit)
     }
   },
   getters: {
-    biggerColumnsLen (state) {
-      return state.columns.filter(c => c.id > 2).length
+    getColumnById: (state) => (id: string) => {
+      return state.columns.find(c => c._id === id)
     },
-    getColumnById: (state) => (id: number) => {
-      return state.columns.find(c => c.id === id)
-    },
-    getPostsByCid: (state) => (cid: number) => {
-      return state.posts.filter(post => post.columnId === cid)
+    getPostsByCid: (state) => (cid: string) => {
+      return state.posts.filter(post => post.column === cid)
     }
   }
 })
